0e022284266a1830f6dca2ccd26a941f15845670
Wyatt Van Amburg <wyatt.va@icloud.com>
2023-09-07 21:09:47 -0700

    Made the addBlob method, updated tester

--- Index.java
+++ Index.java
@@ -1,7 +1,11 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Index {
+    private static HashMap<String, String> hMap = new HashMap<>();
     public static void init() throws IOException
     {
         File file = new File("index");
@@ -16,10 +20,17 @@ public class Index {
             dir.mkdir();
         }
     }
-    public static void addBlob(String path) throws IOException
+    public static void addBlob(String fileName) throws IOException
     {
         init();
-
-        
+        String hash = Blob.encryptThisString(fileName);
+        if (hash != null)
+        {
+            hMap.put(fileName, hash);
+        }
+        BufferedWriter bw = new BufferedWriter(new FileWriter("index", true));
+        bw.write(fileName + " : " + hash);
+        bw.write("\n");
+        bw.close();
     }
 }
--- main.java
+++ main.java
@@ -4,8 +4,7 @@ public class main {
     public static void main (String[] args) throws IOException
     {
         Index.init();
-        Blob b = new Blob();
-        b.writeToObjects("testFile.txt");
+        Index.addBlob("testFile.txt");
     }
    
     
