0c496d5246351211d7af08ae15ad74d9381798af
Wyatt Van Amburg <wyatt.va@icloud.com>
2023-09-28 22:36:07 -0700

    tester

--- addDirectoryTest.java
+++ addDirectoryTest.java
@@ -32,7 +32,36 @@ public class addDirectoryTest {
 
         //System.out.println("Tree contents: ");
         //System.out.println(mainTree.getTree());
-        String expected = "3c760db4248d9dd38c13c9eea0f1c0bddea2fc68";
+        String expected = "02ab9869557ee5b2e8068ac153ddf3bd12d10674";
         assertEquals(expected, result);
+    }
+
+    void createSubDirectoryWithFiles(File parentDir, String subDirName) {
+        File subDir = new File(parentDir, subDirName);
+        subDir.mkdir();
+
+        // Create files within the subdirectory (you can add more as needed)
+        File file1 = new File(subDir, "file4.txt");
+        File file2 = new File(subDir, "file5.txt");
+        try {
+            createFile(file1.getPath(), "asdfg");
+            createFile(file2.getPath(), "qwerty");
+        } catch (IOException e) {
+            fail("Failed to create files: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testAddDirectoryWithSubFolders() throws Exception{
+        File dir = new File("directoryTestFile");
+        dir.mkdir();
+        createFile("directoryTestFile/file1.txt", "abcdefg");
+        createFile("directoryTestFile/file2.txt", "hijklmn");
+        createFile("directoryTestFile/file3.txt", "opqrstu");
+        createSubDirectoryWithFiles(dir, "subDirectory");
+        tree mainTree = new tree();
+        String result = mainTree.addDirectory("directoryTestFile");
+        String expected = mainTree.encryptThisString(mainTree.getTree());
+        assertEquals(expected,result);
     }
 }
